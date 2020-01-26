79d0f31b8e93291579b43035bb9901e6495f0d21
Evan Lindquist <evanl3@illinois.edu>
2020-01-25 18:22:30 -0600

    added comments and removed serial monitor

--- winch/winch.ino
+++ winch/winch.ino
@@ -1,32 +1,29 @@
 #include <Encoder.h>
 #include <Servo.h>
 
-Servo myservo;
-Encoder myenc(2, 3);
-long oldPosition  = -999;
-long target[] = {100,200,300,400,500};
-long done = 0;
-int i = 0;
+Servo myservo; // create servo object
+Encoder myenc(2, 3); // create encoder object
+long oldPosition  = -999; // initialize an old position
+long target[] = {100,200,300,400,500};  // array of target to control descent speed of servo
+long done = target[sizeof(target) / sizeof(target[0]) - 1]; // intialize a variable for when to end project
+int i = 0; // initialize index for the array
 void setup() { 
-    myservo.attach(9);
-    Serial.begin(9600);
-    myenc.write(0);
-    myservo.write(90);
-    done = target[sizeof(target) / sizeof(target[0]) - 1];
+    myservo.attach(9); // attach servo to Arduino pin
+    myservo.write(90); // initialize servo speed
+    myenc.write(0); // write the base value to the encoder
  } 
  
  void loop() {
-    long newPosition = myenc.read();
+    long newPosition = myenc.read(); // create instance of variable for new encoder position
     if (newPosition != oldPosition) {
-        oldPosition = newPosition;
-        // Serial.println(newPosition);
+        oldPosition = newPosition; // set the current position of encoder to old position for next loop
     }
     if(newPosition >= target[i]) {
-       myservo.write(87);
+       myservo.write(87); // turn off servo
        if(newPosition < done) {
-         i++;
-         delay(1000);
-         myservo.write(90);
+         i++; // increment target index
+         delay(1000); // delay, replace with adequete action
+         myservo.write(90); // turn on servo
        }
     } 
  }
