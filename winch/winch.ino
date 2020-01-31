d668e34f2f59ce7e063659fc5ef8962ee7c5d523
mayachatt <44557298+mayachatt@users.noreply.github.com>
2020-01-31 17:19:34 -0600

    Update winch.ino

--- winch/winch.ino
+++ winch/winch.ino
@@ -11,18 +11,24 @@ void setup() {
     myservo.attach(9); // attach servo to Arduino pin
     myservo.write(90); // initialize servo speed
     myenc.write(0); // write the base value to the encoder
+    Serial.begin(9600);
+    Serial.println("Servo and Rotary Encoder Test");
  } 
  
  void loop() {
     long newPosition = myenc.read(); // create instance of variable for new encoder position
     if (newPosition != oldPosition) {
         oldPosition = newPosition; // set the current position of encoder to old position for next loop
+        Serial.println(newPosition);
     }
     if(newPosition >= target[i]) {
        myservo.write(87); // turn off servo
+       Serial.print("PAUSED, reached target ");
+       Serial.print(target[i]);
+       Serial.println();
        if(newPosition < done) {
          i++; // increment target index
-         delay(1000); // delay, replace with adequete action
+         delay(100); // delay, replace with adequate action
          myservo.write(90); // turn on servo
        }
     } 
