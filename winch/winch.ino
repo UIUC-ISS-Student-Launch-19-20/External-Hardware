859e40c347ec39b34adea1381fca3e49e56e0f09
mayachatt <44557298+mayachatt@users.noreply.github.com>
2020-01-31 18:13:40 -0600

    Update winch.ino

--- winch/winch.ino
+++ winch/winch.ino
@@ -4,9 +4,9 @@
 Servo myservo; // create servo object
 Encoder myenc(2, 3); // create encoder object
 long oldPosition  = -999; // initialize an old position
-long target[] = {100,200,300,400,500};  // array of target to control descent speed of servo
-long done = target[sizeof(target) / sizeof(target[0]) - 1]; // intialize a variable for when to end project
-int i = 0; // initialize index for the array
+long finalTarget = 1000; // final descent target
+long target = -1;
+
 void setup() { 
     myservo.attach(9); // attach servo to Arduino pin
     myservo.write(90); // initialize servo speed
@@ -21,14 +21,11 @@ void setup() {
         oldPosition = newPosition; // set the current position of encoder to old position for next loop
         Serial.println(newPosition);
     }
-    if(newPosition >= target[i]) {
+    if(newPosition >= target) {
        myservo.write(87); // turn off servo
-       Serial.print("PAUSED, reached target ");
-       Serial.print(target[i]);
-       Serial.println();
-       if(newPosition < done) {
-         i++; // increment target index
-         delay(100); // delay, replace with adequate action
+       if(newPosition < finalTarget) {
+         target += 10; // increment target index
+         delay(10); // delay, replace with adequate action
          myservo.write(90); // turn on servo
        }
     } 
