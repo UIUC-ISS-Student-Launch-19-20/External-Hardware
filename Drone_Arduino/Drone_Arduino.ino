6ffba1539c787c7223c4b18a748740d2db194b31
mayachatt <44557298+mayachatt@users.noreply.github.com>
2020-02-21 20:39:41 -0600

    Create Drone_Arduino.ino

---
+++ Drone_Arduino/Drone_Arduino.ino
@@ -0,0 +1,41 @@
+#define burnwire_arms = 2
+#define burnwire_winch = 3
+
+#define photoresistor1 = 4
+#define photoresistor2 = 5
+
+#define arm1 6
+#define arm2 7
+#define arm3 8
+#define arm4 9
+#define irma 10
+
+#define go_in 11
+#define go_out 12
+
+//control variables
+int arms_deploy = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(burnwire_arms, OUTPUT);
+    pinMode(burnwire_winch, OUTPUT);
+    pinMode(photoresistor1, INPUT);
+    pinMode(photoresistor2, INPUT);
+    pinMode(arm1, INPUT);
+    pinMode(arm2, INPUT);
+    pinMode(arm3, INPUT);
+    pinMode(arm4, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(! arms_deployed) {
+      if(photoresistor1 | photoresistor2) {
+          digitalWrite(burnwire_arms, HIGH);
+          arms_deploy = 1;
+      }
+  }
+  if(
+  
+}
